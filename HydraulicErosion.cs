beae3bd808dbc1ae4c0c0c83629403976ce31119
William C. Donaldson <dson.bill@gmail.com>
2017-03-04 01:47:19 -0700

    Remove Odd Characters

--- HydraulicErosion.cs
+++ HydraulicErosion.cs
@@ -1,4 +1,4 @@
-﻿// Fast Hydraulic Erosion Simulation and Visualization on GP
+// Fast Hydraulic Erosion Simulation and Visualization on GP
 
 
 // Development Credits
@@ -42,7 +42,8 @@
 // AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-// THE SOFTWARE.
+// THE SOFTWARE.
+
 
 using UnityEngine;
 
